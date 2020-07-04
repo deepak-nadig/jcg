c7f3b18e9c649a33dedecdefa373bebdb8ab9538
Nadig <nadigd@8c85906d2a6d.ant.amazon.com>
2020-07-03 20:02:15 -0700

    added 2 lines

--- src/com/jcg/SayHello.java
+++ src/com/jcg/SayHello.java
@@ -3,7 +3,9 @@ package com.jcg;
 public class SayHello {
     public static void main(String[] args) {
         System.out.println("Hello there!");
+        System.out.println("Printing even numbers");
         EvenNumbers.printEvenNumber(11);
+        System.out.println("Printing odd numbers");
         OddNumbers.printOddNumber(12);
     }
 }
