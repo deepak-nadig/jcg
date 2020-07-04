34222cfe9bab316206144cf0cd6616a8aab32887
Nadig <nadigd@8c85906d2a6d.ant.amazon.com>
2020-07-03 19:39:20 -0700

    commit-2

--- src/com/jcg/SayHello.java
+++ src/com/jcg/SayHello.java
@@ -3,5 +3,7 @@ package com.jcg;
 public class SayHello {
     public static void main(String[] args) {
         System.out.println("Hello there!");
+        EvenNumbers.printEvenNumber(11);
+        OddNumbers.printOddNumber(12);
     }
 }
